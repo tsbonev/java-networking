86fac70cc0965d463e7e8d470527a57112e58981
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-15 11:53:52 +0300

    Add tests for downloading

--- src/main/java/com/clouway/downloadagent/DownloadAgent.java
+++ src/main/java/com/clouway/downloadagent/DownloadAgent.java
@@ -1,7 +1,51 @@
 package com.clouway.downloadagent;
 
+import com.sun.org.apache.xerces.internal.util.URI;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DownloadAgent {
 
 
+    private URL url;
 
+    public boolean setUrl(String s) throws MalformedURLException {
+
+        this.url = new URL(s);
+        return true;
+
+    }
+
+    public boolean setUrl(URL u) throws MalformedURLException {
+
+        if(null == u){
+            throw new MalformedURLException();
+        }
+
+        this.url = u;
+        return true;
+
+    }
+
+    public boolean setUrl(File f) throws MalformedURLException {
+
+        if(!f.exists()) return false;
+
+        this.url = f.toURI().toURL();
+        return true;
+
+    }
+
+    public File download() throws IOException {
+
+        File downloadedFile = new File("/home/clouway/workspaces/idea/networking/src/test/resources/download.txt");
+
+        FileUtils.copyURLToFile(url, downloadedFile);
+
+        return downloadedFile;
+    }
 }
--- src/test/java/com/clouway/downloadagent/DownloadAgentTest.java
+++ src/test/java/com/clouway/downloadagent/DownloadAgentTest.java
@@ -1,4 +1,83 @@
 package com.clouway.downloadagent;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class DownloadAgentTest {
+
+    DownloadAgent agent = new DownloadAgent();
+
+    @Test
+    public void acceptValidUrlString() throws MalformedURLException {
+
+        assertThat(agent.setUrl("http://www.example.com"), is(true));
+
+    }
+
+    @Test(expected = MalformedURLException.class)
+    public void rejectInvalidUrlString() throws MalformedURLException{
+
+        agent.setUrl("this is not an url");
+
+    }
+
+    @Test
+    public void acceptValidUrlObject() throws MalformedURLException {
+
+        URL url = new URL("http://www.example.com");
+
+        assertThat(agent.setUrl(url), is(true));
+
+    }
+
+    @Test (expected = MalformedURLException.class)
+    public void rejectInvalidUrlObject() throws MalformedURLException, URISyntaxException {
+
+        URL url = null;
+
+        assertThat(agent.setUrl(url), is(false));
+
+    }
+
+    @Test
+    public void acceptFile() throws MalformedURLException {
+
+        File file = new File("/home/clouway/workspaces/idea/networking/src/test/resources/text.txt");
+
+        assertThat(agent.setUrl(file), is(true));
+
+    }
+
+    @Test
+    public void rejectNonExistentFile() throws MalformedURLException{
+
+        File file = new File("non-existent-text.txt");
+
+        assertThat(agent.setUrl(file), is(false));
+
+    }
+
+    @Test
+    public void downloadFile() throws IOException {
+
+        File file = new File("/home/clouway/workspaces/idea/networking/src/test/resources/text.txt");
+
+        agent.setUrl(file);
+
+        File download = agent.download();
+
+        assertThat(FileUtils.contentEquals(file, download), is(true));
+
+
+    }
+
 }
